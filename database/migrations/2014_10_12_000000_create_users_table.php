f1367b36cb13e6bf3b8c618b9ece8aa4e6033b72
SatrioRama <satriorama1233@gmail.com>
2021-02-04 15:59:01 +0700

    fix: error crate table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,14 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id');
+            $table->primary('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('active')->default(1);
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
