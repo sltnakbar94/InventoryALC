8983848954ecfa16d97d4e1920a5cadbf558f99b
fahri <saya.masfahri@gmail.com>
2021-02-11 14:24:29 +0700

    Fix: Relation model_has_roles

--- database/migrations/2021_02_10_113937_create_permission_tables.php
+++ database/migrations/2021_02_10_113937_create_permission_tables.php
@@ -58,7 +58,7 @@ class CreatePermissionTables extends Migration
             $table->unsignedBigInteger('role_id');
 
             $table->string('model_type');
-            $table->unsignedBigInteger($columnNames['model_morph_key']);
+            $table->string($columnNames['model_morph_key']);
             $table->index([$columnNames['model_morph_key'], 'model_type'], 'model_has_roles_model_id_model_type_index');
 
             $table->foreign('role_id')
