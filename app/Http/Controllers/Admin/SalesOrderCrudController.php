47c12d9df108d1ae5902cfb890427fee58dd6537
SatrioRama <satriorama1233@gmail.com>
2021-03-05 21:06:00 +0700

    test

--- app/Http/Controllers/Admin/SalesOrderCrudController.php
+++ app/Http/Controllers/Admin/SalesOrderCrudController.php
@@ -51,6 +51,7 @@ class SalesOrderCrudController extends CrudController
         if (backpack_user()->hasRole('sales')) {
             $this->crud->addClause('where', 'user_id', '=', backpack_auth()->id());
         }
+        $this->crud->addClause('where', 'status', '=', 0);
         if (backpack_user()->hasRole('purchasing')) {
             $this->crud->removeButton('create');
             $this->crud->removeButton('update');
--- app/Http/Controllers/Admin/WarehouseInCrudController.php
+++ app/Http/Controllers/Admin/WarehouseInCrudController.php
@@ -58,6 +58,7 @@ class WarehouseInCrudController extends CrudController
         if (backpack_user()->hasRole('sales')) {
             $this->crud->addClause('where', 'user_id', '=', backpack_auth()->id());
         }
+        $this->crud->addClause('where', 'status', '=', 0);
         if (backpack_user()->hasAnyRole(['purchasing', 'operator-gudang'])) {
             $this->crud->removeButton('create');
             $this->crud->removeButton('update');
