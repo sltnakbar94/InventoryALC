0deb576697ba56f62e5dd5229c2617cba0083268
SULTANOMN\sultanakbar <sltn.akbar94@gmail.com>
2021-03-17 14:33:36 +0700

    tambah data item modal

--- resources/views/warehouse/sf/add-master-item.blade.php
+++ resources/views/warehouse/sf/add-master-item.blade.php
@@ -33,36 +33,44 @@
                         </div>
                         <div class="form-group">
                             <label class="control-label" for="uom">Satuan</label><br>
-                            <select name="uom" id="uom" class="form-control{{ $errors->has('uom') ? ' is-invalid' : '' }} select2" style="width: 100%" required>
+                            <select name="uom" id="uom" onchange="changeFunc();" class="form-control{{ $errors->has('uom') ? ' is-invalid' : '' }} select2" style="width: 100%" required>
                                 <option value="">--PILIH SATUAN--</option>
                                 @foreach(\App\Models\Unit::get() as $value => $text)
                                     <option value="{{ $text->id }}">{{ $text->name }}</option>
                                 @endforeach
-                                {{-- <a href="{{ backpack_url('unit/create') }}">Tambah Satuan</a> --}}
+                                <option value="NewItem" >Tambah Satuan</option>
                             </select>
-                            <a href="{{ backpack_url('unit/create') }}" data-toggle="modal" style="float: left">Satuan tidak ada ?</a>
+
+                            <div class="form-group mt-2" style="display:none" id="AddItem">
+                                <input type="text" class="form-control" id="exampleFormControlInput" name="" placeholder="Input Satuan">
+                            </div>
                         </div>
                         <div class="form-group">
                             <label class="control-label" for="category">Kategori</label><br>
-                            <select name="category" id="category" class="form-control{{ $errors->has('category') ? ' is-invalid' : '' }} select2" style="width: 100%" required>
+                            <select name="category" id="category" onchange="AddCategory();" class="form-control{{ $errors->has('category') ? ' is-invalid' : '' }} select2" style="width: 100%" required>
                                 <option value="">--PILIH KATEGORI--</option>
                                 @foreach(\App\Models\Category::get() as $value => $text)
                                     <option value="{{ $text->id }}">{{ $text->name }}</option>
                                 @endforeach
-                                {{-- <a href="{{ backpack_url('category/create') }}">Tambah Kategori</a> --}}
+                                <option value="AddCategory">Tambah Kategori</option>
                             </select>
-                            <a href="{{ backpack_url('category/create') }}" data-toggle="modal" style="float: left">Kategori tidak ada ?</a>
+                             <div class="form-group mt-2" style="display:none" id="addCategory">
+                                <input type="text" class="form-control" id="newCategory" name="" placeholder="Input Kategori">
+                            </div>
                         </div>
                         <div class="form-group">
                             <label class="control-label" for="brand">Brand</label><br>
-                            <select name="brand" id="brand" class="form-control{{ $errors->has('brand') ? ' is-invalid' : '' }} select2" style="width: 100%" required>
+                            <select name="brand" id="brand" onchange="AddBrand()" class="form-control{{ $errors->has('brand') ? ' is-invalid' : '' }} select2" style="width: 100%" required>
                                 <option value="">--PILIH BRAND--</option>
                                 @foreach(\App\Models\Brand::get() as $value => $text)
                                     <option value="{{ $text->id }}">{{ $text->name }}</option>
                                 @endforeach
+                                <option value="AddBrand">Tambah Brand</option>
                                 {{-- <a href="{{ backpack_url('brand/create') }}">Tambah Brand</a> --}}
                             </select>
-                            <a href="{{ backpack_url('brand/create') }}" data-toggle="modal" style="float: left">Brand tidak ada ?</a>
+                            <div class="form-group mt-2" style="display:none" id="addBrand">
+                                <input type="text" class="form-control" id="newBrand" name="" placeholder="Tambah Brand">
+                            </div>
                         </div>
                         <div class="form-group">
                             <label class="control-label" for="qty">Jumlah</label>
@@ -84,3 +92,41 @@
     </div>
 </div>
 
+<script type="text/javascript">
+
+    function changeFunc() {
+     var selectBox = document.getElementById("uom");
+     var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+    var addItem = document.getElementById("AddItem")
+     if(selectedValue == "NewItem"){
+        addItem.style.display = "block" ;
+     }else{
+        addItem.style.display = "none" ;
+     }
+    }
+
+    function AddCategory(){
+    var newCategory = document.getElementById("category");
+    var selectedValues = newCategory.options[newCategory.selectedIndex].value;
+    var addCategory = document.getElementById("addCategory");
+    if(selectedValues == "AddCategory"){
+        addCategory.style.display = "block";
+    }else{
+        addCategory.style.display = "none" ;
+    }
+    }
+
+    function AddBrand(){
+        var newBrand = document.getElementById("brand");
+        var selectedValue = newBrand.options[newBrand.selectedIndex].value ;
+        var addBrand = document.getElementById("addBrand");
+        if(selectedValue == "AddBrand"){
+            addBrand.style.display = "block";
+        }else{
+            addBrand.style.display = "none"
+        }
+    }
+
+
+
+   </script>
