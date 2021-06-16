a64c7f243689d4219a50b0398aece0536d95d276
omen-omen\sultanomn <sltn.akbar94@gmail.com>
2021-06-16 16:26:45 +0700

    logo

--- resources/views/vendor/backpack/base/auth/login.blade.php
+++ resources/views/vendor/backpack/base/auth/login.blade.php
@@ -4,10 +4,10 @@
     <div class="row justify-content-center">
         <div class="col-12 col-md-8 col-lg-4">
             <!-- <h3 class="text-center mb-4">{{ trans('backpack::base.login') }}</h3> -->
-            <h3 class="text-center mb-4 mt-3">
-                <img src="/logo/ALA.jpeg" alt="CMS" style="width:200px;height:auto;">
-            </h3>
             <div class="card">
+                <h3 class="text-center mb-4 mt-3">
+                    <img src="/logo/ALA.jpeg" alt="CMS" style="width:200px;height:auto;">
+                </h3>
                 <div class="card-body">
                     <form class="col-md-12 p-t-10" role="form" method="POST" action="{{ route('backpack.auth.login') }}">
                         {!! csrf_field() !!}
