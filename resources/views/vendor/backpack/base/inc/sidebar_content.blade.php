72ab82f7a2b6bec392c77e35d6c1a2b2b1f3a8f0
omen-omen\sultanomn <sltn.akbar94@gmail.com>
2021-07-05 15:07:19 +0700

    sidebar update

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,14 +1,14 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="las la-tachometer-alt"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 @if(backpack_user()->hasRole('sales'))
-    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('submissionform') }}'><i class='lar la-file-alt'></i> Purchase Requisition</a></li>
+    {{-- <li class='nav-item'><a class='nav-link' href='{{ backpack_url('submissionform') }}'><i class='lar la-file-alt'></i> Purchase Requisition</a></li>
     <li class="nav-item nav-dropdown">
         <a class="nav-link nav-dropdown-toggle" href="#"><i class="las la-industry"></i> sales</a>
         <ul class="nav-dropdown-items">
             <li class='nav-item'><a class='nav-link' href='{{ backpack_url('salesorder') }}'><i class="lar la-file-alt"></i> Sales Order</a></li>
             <li class='nav-item'><a class='nav-link' href='{{ backpack_url('deliverybysales') }}'><i class='lar la-file'></i> Delivery By Sales</a></li>
         </ul>
-    </li>
+    </li> --}}
     
     {{-- <i class="nav-item nav-dropdown">
         <a class="nav-link nav-dropdown-toggle" href="#"><i class="las la-industry"></i> Warehouse</a> --}}
