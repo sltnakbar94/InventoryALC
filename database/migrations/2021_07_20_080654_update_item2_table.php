e6940ea24cddcc83cef6fb74a786f5aee3eafffa
JARVISDESKTOP <satriorama1233@gmail.com>
2021-07-20 16:17:24 +0700

    update; salah data wkwkwk

--- database/migrations/2021_07_20_080654_update_item2_table.php
+++ database/migrations/2021_07_20_080654_update_item2_table.php
@@ -14,7 +14,7 @@ class UpdateItem2Table extends Migration
     public function up()
     {
         Schema::table('items', function(Blueprint $table) {
-            $table->string('sample')->nullable();
+            $table->boolean('sample')->default(0);
         });
     }
 
