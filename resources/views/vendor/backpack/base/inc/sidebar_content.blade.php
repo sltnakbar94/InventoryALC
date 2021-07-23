67729bc5fc3225273a89d8a1036b39b8a87c9730
Ramadhanu-Lenovo <satriorama1233@gmail.com>
2021-07-23 16:52:05 +0700

    update: repihin sidebar

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,115 +1,30 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="las la-tachometer-alt"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 @if(backpack_user()->hasRole('sales'))
-    {{-- <li class='nav-item'><a class='nav-link' href='{{ backpack_url('submissionform') }}'><i class='lar la-file-alt'></i> Purchase Requisition</a></li>
-    <li class="nav-item nav-dropdown">
-        <a class="nav-link nav-dropdown-toggle" href="#"><i class="las la-industry"></i> sales</a>
-        <ul class="nav-dropdown-items">
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('salesorder') }}'><i class="lar la-file-alt"></i> Sales Order</a></li>
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('deliverybysales') }}'><i class='lar la-file'></i> Delivery By Sales</a></li>
-        </ul>
-    </li> --}}
-
-    {{-- <i class="nav-item nav-dropdown">
-        <a class="nav-link nav-dropdown-toggle" href="#"><i class="las la-industry"></i> Warehouse</a> --}}
-            {{-- <li class='nav-item'><a class='nav-link' href='{{ backpack_url('warehousein') }}'><i class="lar la-file-alt"></i> Purchase Order</a></li> --}}
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('warehouseout') }}'><i class="lar la-file"></i> Delivery Order</a></li>
-            {{-- <li class='nav-item'><a class='nav-link' href='{{ backpack_url('goodreceive') }}'><i class='las la-industry'></i> Terima Barang</a></li> --}}
-        {{-- </ul>
-    </li> --}}
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('warehouseout') }}'><i class="lar la-file"></i> Delivery Order</a></li>
     <li class='nav-item'><a class='nav-link' href='{{ backpack_url('stackholder') }}'><i class="las la-list-ol"></i> Stackholders</a></li>
-    {{-- <li class='nav-item'><a class='nav-link' href='{{ backpack_url('deliverynote') }}'><i class="lar la-file"></i> Delivery Note</a></li> --}}
     <li class="nav-item nav-dropdown">
         <a class="nav-link nav-dropdown-toggle" href="#"><i class="las la-folder"></i> Report</a>
         <ul class="nav-dropdown-items">
-            {{-- <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportpurchaserequisition') }}'><i class='lar la-file-alt'></i> ReportPurchaseRequisitions</a></li>
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportsalesorder') }}'><i class="lar la-file-alt"></i> ReportSalesOrders</a></li>
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportdeliverybysales') }}'><i class='lar la-file'></i> ReportDeliveryBySales</a></li>
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportpurchaseorder') }}'><i class="lar la-file-alt"></i> ReportPurchaseOrders</a></li> --}}
             <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportdeliveryorder') }}'><i class="lar la-file-alt"></i> Delivery Orders</a></li>
             <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportdeliverynote') }}'><i class="lar la-file-alt"></i> Delivery Notes</a></li>
         </ul>
     </li>
-    {{-- <li class='nav-item'><a class='nav-link' href='{{ backpack_url('invoice') }}'><i class="lar la-file-alt"></i> Invoices</a></li> --}}
-@endif
-@if(backpack_user()->hasRole('purchasing'))
-    {{-- <li class="nav-item nav-dropdown">
-        <a class="nav-link nav-dropdown-toggle" href="#"><i class="las la-industry"></i> sales</a>
-        <ul class="nav-dropdown-items">
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('salesorder') }}'><i class="lar la-file-alt"></i> Sales Order</a></li>
-        </ul>
-    </li> --}}
-    {{-- <li class="nav-item nav-dropdown">
-        <a class="nav-link nav-dropdown-toggle" href="#"><i class="las la-industry"></i> Warehouse</a>
-        <ul class="nav-dropdown-items"> --}}
-            {{-- <li class='nav-item'><a class='nav-link' href='{{ backpack_url('warehousein') }}'><i class="lar la-file-alt"></i> Purchase Order</a></li> --}}
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('warehouseout') }}'><i class="lar la-file"></i> Delivery Order</a></li>
-            {{-- <li class='nav-item'><a class='nav-link' href='{{ backpack_url('goodreceive') }}'><i class='las la-industry'></i> Terima Barang</a></li> --}}
-        {{-- </ul> --}}
-    {{-- </li> --}}
-
-    <li class="nav-item nav-dropdown">
-        <a class="nav-link nav-dropdown-toggle" href="#"><i class="las la-folder"></i> Report</a>
-        <ul class="nav-dropdown-items">
-            {{-- <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportpurchaserequisition') }}'><i class='lar la-file-alt'></i> ReportPurchaseRequisitions</a></li>
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportsalesorder') }}'><i class="lar la-file-alt"></i> ReportSalesOrders</a></li>
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportdeliverybysales') }}'><i class='lar la-file'></i> ReportDeliveryBySales</a></li>
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportpurchaseorder') }}'><i class='nav-icon la la-question'></i> ReportPurchaseOrders</a></li> --}}
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportdeliveryorder') }}'><i class='lar la-file-alt'></i> Delivery Orders</a></li>
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportdeliverynote') }}'><i class='lar la-file-alt'></i> Delivery Notes</a></li>
-        </ul>
-    </li>
 @endif
 @if(backpack_user()->hasRole('operator-gudang'))
-    {{-- <li class='nav-item'><a class='nav-link' href='{{ backpack_url('warehousein') }}'><i class="lar la-file-alt"></i> Terima Barang</a></li> --}}
     <li class='nav-item'><a class='nav-link' href='{{ backpack_url('goodreceive') }}'><i class='las la-industry'></i> Terima Barang</a></li>
     <li class='nav-item'><a class='nav-link' href='{{ backpack_url('deliverynote') }}'><i class="lar la-file"></i> Surat Jalan</a></li>
     <li class="nav-item nav-dropdown">
         <a class="nav-link nav-dropdown-toggle" href="#"><i class="las la-folder"></i> Report</a>
         <ul class="nav-dropdown-items">
-            {{-- <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportpurchaserequisition') }}'><i class='lar la-file-alt'> ReportPurchaseRequisitions</a></li>
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportsalesorder') }}'><i class="lar la-file-alt"></i> ReportSalesOrders</a></li>
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportdeliverybysales') }}'><i class='lar la-file'></i></i> ReportDeliveryBySales</a></li>
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportpurchaseorder') }}'><i class='nav-icon la la-question'></i>ReportPurchaseOrders</a></li> --}}
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportdeliveryorder') }}'><i class='lar la-file-alt'></i> Delivery Orders</a></li>
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportdeliverynote') }}'><i class='lar la-file-alt'></i> Delivery Notes</a></li>
+            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportgoodreceive') }}'><i class='nav-icon la la-question'></i> Report Terima Barang</a></li>
+            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportdeliverynote') }}'><i class='lar la-file-alt'></i> Surat Jalan</a></li>
         </ul>
     </li>
 @endif
-@if (backpack_user()->hasAnyRole(['admin', 'superadmin']))
-    {{-- <li class='nav-item'><a class='nav-link' href='{{ backpack_url('submissionform') }}'><i class='lar la-file-alt'></i> Purchase Requisition</a></li>
-    <li class="nav-item nav-dropdown">
-        <a class="nav-link nav-dropdown-toggle" href="#"><i class="las la-industry"></i> sales</a>
-        <ul class="nav-dropdown-items">
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('salesorder') }}'><i class="lar la-file-alt"></i> Sales Order</a></li>
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('deliverybysales') }}'><i class='lar la-file'></i> Delivery By Sales</a></li>
-        </ul>
-    </li>
-    <li class="nav-item nav-dropdown">
-        <a class="nav-link nav-dropdown-toggle" href="#"><i class="las la-industry"></i> Warehouse</a>
-        <ul class="nav-dropdown-items">
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('warehousein') }}'><i class="lar la-file-alt"></i> Purchase Order</a></li>
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('warehouseout') }}'><i class="lar la-file"></i> Delivery Order</a></li>
-        </ul>
-    </li>
-
-    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('deliverynote') }}'><i class="lar la-file"></i> Delivery Note</a></li> --}}
-    <li class="nav-item nav-dropdown">
-        <a class="nav-link nav-dropdown-toggle" href="#"><i class="las la-folder"></i> Report</a>
-        <ul class="nav-dropdown-items">
-            {{-- <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportpurchaserequisition') }}'><i class='lar la-file-alt'> ReportPurchaseRequisitions</a></li>
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportsalesorder') }}'><i class="lar la-file-alt"></i> ReportSalesOrders</a></li>
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportdeliverybysales') }}'><i class="lar la-file-alt"></i> ReportDeliveryBySales</a></li>
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportpurchaseorder') }}'><i class="lar la-file-alt"></i> ReportPurchaseOrders</a></li> --}}
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportgoodreceive') }}'><i class='nav-icon la la-question'></i> Report Terima Barang</a></li>
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportdeliveryorder') }}'><i class='lar la-file-alt'></i> Delivery Orders</a></li>
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportdeliverynote') }}'><i class='lar la-file-alt'></i></i> Delivery Notes</a></li>
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportinvoice') }}'><i class='nav-icon la la-question'></i> Report Invoice</a></li>
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportpiutang') }}'><i class='nav-icon la la-question'></i> Report Piutang</a></li>
-            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportitem') }}'><i class='nav-icon la la-question'></i> Report Item</a></li>
-        </ul>
-    </li>
-    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('invoice') }}'><i class="lar la-file-alt"></i>Invoice</a></li>
+@if(backpack_user()->hasRole('purchasing'))
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('warehouseout') }}'><i class="lar la-file"></i> Delivery Order</a></li>
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('invoice') }}'><i class="lar la-file-alt"></i> Invoice</a></li>
     <li class="nav-item nav-dropdown">
         <a class="nav-link nav-dropdown-toggle" href="#"><i class="las la-project-diagram"></i> Config</a>
         <ul class="nav-dropdown-items">
@@ -123,7 +38,47 @@
         </ul>
     </li>
 @endif
+@if (backpack_user()->hasRole('admin'))
+    <li class="nav-item nav-dropdown">
+        <a class="nav-link nav-dropdown-toggle" href="#"><i class="las la-folder"></i> Report</a>
+        <ul class="nav-dropdown-items">
+            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportgoodreceive') }}'><i class='nav-icon la la-question'></i> Report Terima Barang</a></li>
+            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportdeliveryorder') }}'><i class='lar la-file-alt'></i> Report Delivery Orders</a></li>
+            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportdeliverynote') }}'><i class='lar la-file-alt'></i></i> Report Delivery Notes</a></li>
+            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportinvoice') }}'><i class='nav-icon la la-question'></i> Report Invoice</a></li>
+            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportpiutang') }}'><i class='nav-icon la la-question'></i> Report Piutang</a></li>
+            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportitem') }}'><i class='nav-icon la la-question'></i> Report Item</a></li>
+        </ul>
+    </li>
+@endif
 @if (backpack_user()->hasRole('superadmin'))
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('goodreceive') }}'><i class='las la-industry'></i> Terima Barang</a></li>
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('warehouseout') }}'><i class="lar la-file"></i> Delivery Order</a></li>
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('deliverynote') }}'><i class="lar la-file"></i> Surat Jalan</a></li>
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('invoice') }}'><i class="lar la-file-alt"></i> Invoice</a></li>
+    <li class="nav-item nav-dropdown">
+        <a class="nav-link nav-dropdown-toggle" href="#"><i class="las la-folder"></i> Report</a>
+        <ul class="nav-dropdown-items">
+            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportgoodreceive') }}'><i class='nav-icon la la-question'></i> Report Terima Barang</a></li>
+            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportdeliveryorder') }}'><i class='lar la-file-alt'></i> Report Delivery Orders</a></li>
+            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportdeliverynote') }}'><i class='lar la-file-alt'></i></i> Report Delivery Notes</a></li>
+            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportinvoice') }}'><i class='nav-icon la la-question'></i> Report Invoice</a></li>
+            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportpiutang') }}'><i class='nav-icon la la-question'></i> Report Piutang</a></li>
+            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reportitem') }}'><i class='nav-icon la la-question'></i> Report Item</a></li>
+        </ul>
+    </li>
+    <li class="nav-item nav-dropdown">
+        <a class="nav-link nav-dropdown-toggle" href="#"><i class="las la-project-diagram"></i> Config</a>
+        <ul class="nav-dropdown-items">
+            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('item') }}'><i class="las la-list-ol"></i> Items</a></li>
+            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('category') }}'><i class="las la-list-ol"></i> Categories</a></li>
+            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('brand') }}'><i class="las la-list-ol"></i> Brands</a></li>
+            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('unit') }}'><i class="las la-list-ol"></i> Units</a></li>
+            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('company') }}'><i class="las la-list-ol"></i> Companies</a></li>
+            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('stackholder') }}'><i class="las la-list-ol"></i> Stackholders</a></li>
+            <li class='nav-item'><a class='nav-link' href='{{ backpack_url('gudang') }}'><i class='las la-list-ol'></i> Warehouses</a></li>
+        </ul>
+    </li>
     <li class="nav-item nav-dropdown">
         <a class="nav-link nav-dropdown-toggle" href="#"><i class="las la-user-friends"></i> Authentication</a>
         <ul class="nav-dropdown-items">
