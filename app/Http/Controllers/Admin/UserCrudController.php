a715b231e3d9837c8061d394a42bf5d8976a09cf
JARVISDESKTOP <satriorama1233@gmail.com>
2021-07-28 22:39:48 +0700

    update: config user

--- app/Http/Controllers/Admin/UserCrudController.php
+++ app/Http/Controllers/Admin/UserCrudController.php
@@ -2,116 +2,88 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Requests\UserRequest;
-use Backpack\CRUD\app\Http\Controllers\CrudController;
-use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+use Backpack\PermissionManager\app\Http\Controllers\UserCrudController as BackpackUserCrudController;
+use App\Models\Warehouse;
 
-
-/**
- * Class UserCrudController
- * @package App\Http\Controllers\Admin
- * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
- */
-class UserCrudController extends CrudController
+class UserCrudController extends BackpackUserCrudController
 {
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation{ store as traitStore ;}
-    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation { update as updateStore;}
-    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
-
-    /**
-     * Configure the CrudPanel object. Apply settings to all operations.
-     *
-     * @return void
-     */
-    public function setup()
+    protected function addUserFields()
     {
-        CRUD::setModel(\App\Models\User::class);
-        CRUD::setRoute(config('backpack.base.route_prefix') . '/user');
-        CRUD::setEntityNameStrings('user', 'users');
-    }
-
-    /**
-     * Define what happens when the List operation is loaded.
-     *
-     * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
-     * @return void
-     */
-    protected function setupListOperation()
-    {
-        CRUD::column('name');
-        CRUD::column('email');
-        CRUD::column('password');
-
-        /**
-         * Columns can be defined using the fluent syntax or array syntax:
-         * - CRUD::column('price')->type('number');
-         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']);
-         */
-    }
-
-    /**
-     * Define what happens when the Create operation is loaded.
-     *
-     * @see https://backpackforlaravel.com/docs/crud-operation-create
-     * @return void
-     */
-    protected function setupCreateOperation()
-    {
-        CRUD::setValidation(UserRequest::class);
-
-        CRUD::column('name');
-        CRUD::column('email');
-        CRUD::column('password');
-
-        /**
-         * Fields can be defined using the fluent syntax or array syntax:
-         * - CRUD::field('price')->type('number');
-         * - CRUD::addField(['name' => 'price', 'type' => 'number']));
-         */
-    }
-
-    /**
-     * Define what happens when the Update operation is loaded.
-     *
-     * @see https://backpackforlaravel.com/docs/crud-operation-update
-     * @return void
-     */
-    protected function setupUpdateOperation()
-    {
-        $this->setupCreateOperation();
-    }
-
-    public function store()
-    {
-        $this->crud->setRequest($this->crud->validateRequest());
-        $this->crud->setRequest($this->handlePasswordInput($this->crud->getRequest()));
-        $this->crud->unsetValidation();
-
-        return $this->traitStore();
-    }
-
-    public function update()
-    {
-        $this->crud->setRequest($this->crud->validateRequest());
-        $this->crud->setRequest($this->handlePasswordInput($this->crud->getRequest()));
-        $this->crud->unsetValidation();
-
-        return $this->traitUpdate();
-    }
-
-    public function handlePasswordInput($request)
-    {
-        $request->request->remove('password_confirmation');
-        $request->request->remove('roles_show');
-        $request->request->remove('permissions_show');
-
-        if ($request->input('password')) {
-            $request->request->set('password' , bcrypt($request->input('password')));
-        }else{
-            $request->remove('password');
-        }
-        return $request ;
+        $this->crud->addFields([
+            [
+                'tab'   => 'Profile',
+                'name'  => 'name',
+                'label' => trans('backpack::permissionmanager.name'),
+                'type'  => 'text',
+                'attributes' => [
+                    'required' => true,
+                ]
+            ],
+            [
+                'tab'   => 'Profile',
+                'name'  => 'email',
+                'label' => trans('backpack::permissionmanager.email'),
+                'type'  => 'email',
+                'attributes' => [
+                    'required' => true,
+                ]
+            ],
+            [
+                'tab'   => 'Profile',
+                'name'  => 'password',
+                'label' => trans('backpack::permissionmanager.password'),
+                'type'  => 'password',
+                'attributes' => [
+                    'required' => true,
+                ]
+            ],
+            [
+                'tab'   => 'Profile',
+                'name'  => 'password_confirmation',
+                'label' => trans('backpack::permissionmanager.password_confirmation'),
+                'type'  => 'password',
+                'attributes' => [
+                    'required' => true,
+                ]
+            ],
+            [
+                'tab' => 'Profile',
+                'name' => 'warehouse_id',
+                'label' => 'Gudang',
+                'type' => 'select_from_array',
+                'options' => Warehouse::pluck('name', 'id'),
+                'allows_null' => true,
+            ],
+            [
+                'tab'               => 'Profile',
+                // two interconnected entities
+                'label'             => 'Roles',
+                'field_unique_name' => 'user_role_permission',
+                'type'              => 'checklist_dependency',
+                'name'              => ['roles', 'permissions'],
+                'subfields'         => [
+                    'primary' => [
+                        'label'            => '',
+                        'name'             => 'roles', // the method that defines the relationship in your Model
+                        'entity'           => 'roles', // the method that defines the relationship in your Model
+                        'entity_secondary' => 'permissions', // the method that defines the relationship in your Model
+                        'attribute'        => 'name', // foreign key attribute that is shown to user
+                        'model'            => config('permission.models.role'), // foreign key model
+                        'pivot'            => true, // on create&update, do you need to add/delete pivot table entries?]
+                        'number_columns'   => 3, //can be 1,2,3,4,6
+                    ],
+                    'secondary' => [
+                        'label'          => '',
+                        'name'           => 'permissions', // the method that defines the relationship in your Model
+                        'entity'         => 'permissions', // the method that defines the relationship in your Model
+                        'entity_primary' => 'roles', // the method that defines the relationship in your Model
+                        'attribute'      => 'name', // foreign key attribute that is shown to user
+                        'model'          => config('permission.models.permission'), // foreign key model
+                        'pivot'          => true, // on create&update, do you need to add/delete pivot table entries?]
+                        'number_columns' => 3, //can be 1,2,3,4,6
+                    ],
+                ],
+            ],
+        ]);
     }
 }
