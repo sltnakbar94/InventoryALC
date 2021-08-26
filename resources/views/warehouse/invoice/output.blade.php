54a087317de0a774677887df27494e3b527870b2
Legion Irvanda <irvandaauldrien@gmail.com>
2021-08-26 11:40:08 +0700

    Update: Output invoice kepala gudang bogor

--- resources/views/warehouse/invoice/output.blade.php
+++ resources/views/warehouse/invoice/output.blade.php
@@ -200,7 +200,7 @@
 @else
     <table cellspacing="0" border="0" width="100%">
         <tr>
-            <td width="70" style="border-top: 1px solid #000000" colspan=2 align="center" valign=bottom><u><font face="Trebuchet MS" size=1 color="#000000">Agus Saepuloh</font></u></td>
+            <td width="70" style="border-top: 1px solid #000000" colspan=2 align="center" valign=bottom><u><font face="Trebuchet MS" size=1 color="#000000">Agung Herwandi</font></u></td>
             <td width="20" align="left" valign=bottom><font face="Trebuchet MS" size=1 color="#000000"><br></font></td>
             <td width="70" style="border-top: 1px solid #000000" colspan=2 align="center" valign=bottom><u><font face="Trebuchet MS" size=1 color="#000000">Mudasir</font></u></td>
             <td width="20" align="left" valign=bottom><font face="Trebuchet MS" size=1 color="#000000"><br></font></td>
